320e1899efbe0a9eeeb23401c87a6ae2efcd727a
Boogii <bb5407473@gmail.com>
2022-03-07 01:09:47 +0800

    odo jinken shaana

---
+++ SET.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ 
+public class SET {
+       public static void main(String[] args)
+    {
+  
+        Set<String> hash_Set = new HashSet<String>();
+
+        hash_Set.add("lab");
+        hash_Set.add("jishee");
+        hash_Set.add("set");
+        System.out.println(hash_Set.toString());
+    }
+}
